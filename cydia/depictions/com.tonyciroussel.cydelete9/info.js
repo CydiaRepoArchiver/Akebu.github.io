7420ed7a7dadb72a7c5a71f0bc82525519e17c4a
Tony Ciroussel <tony.ciroussel@riseup.net>
2016-12-31 00:16:34 +0100

    Updated languages & depictions

--- cydia/depictions/com.tonyciroussel.cydelete9/info.js
+++ cydia/depictions/com.tonyciroussel.cydelete9/info.js
@@ -1,9 +1,9 @@
 var description = "Cydelete lets you uninstall Cydia applications directly from the SpringBoard. All you need is just hold an application and hit the X button<br/>You can as well hide every Apple's applications in the same way"
 addView("Description", "", description)
-addView("What's new ?", "29 Dec. 2016", "Stable version")
+addView("What's new ?", "30 Dec. 2016", "- Arabic & Dutch translations<br>- Fixed a bug")
 
 var object = ["Author", "Category", "Updated","Version","Compatibility","Languages","Dependencies"];
-var key = ["Tony Ciroussel","Tweaks","29 Dec. 2016","1.0 Beta","iPhone, iPad, iPod with iOS 9.0 or greater, not compatible with iOS 10", "English, Français", "mobilesubstrate, sudo, sed, preferenceloader"];
+var key = ["Tony Ciroussel","Tweaks","29 Dec. 2016","1.0 Beta","iPhone, iPad, iPod with iOS 9.0 or greater, not compatible with iOS 10", "English, Français, العربية, Nederlands", "mobilesubstrate, sudo, sed, preferenceloader"];
 addViewWithTable("Informations", object, key)
 
 addScreenshotButton()
