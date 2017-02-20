23d62871b7ee352d5fd3a632ea868b9af8ad47ac
Tony Ciroussel <tony.ciroussel@riseup.net>
2017-02-21 00:42:52 +0100

    Updated sensible

--- cydia/depictions/com.tonyciroussel.sensible/changelog.js
+++ cydia/depictions/com.tonyciroussel.sensible/changelog.js
@@ -1,2 +1,3 @@
+addView("1.1", "21 Feb. 2017", "- Clean up code<br>- Bug fixes<br>")
 addView("1.0", "10 Feb. 2017", "- Stable version<br>- Preference pane added<br>- Bug fixes<br>")
 addView("0.1 Alpha", "16 Jan. 2017", "- First version<br>- Sensible is working and tested on iOS 9<br>- Give an sweet haptic feedback<br>I will add a preference pane for the next update")
